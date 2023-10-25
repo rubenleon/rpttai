4b63305c5ed8e51ef36eb196289e01aa9cb202fd
Rubén León <ruben2.leon@gmail.com>
2023-10-25 19:59:48 +0200

    Ajuste

--- js/script.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-    .then(reg => console.log('Registro de SW exitoso', reg))
-    .catch(err => console.warn('Error al tratar de registrar el sw', err))
-}
