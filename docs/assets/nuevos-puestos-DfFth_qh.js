8c13da7f1421037d014cf748cbea23d13619cb78
Rubén León <ruben2.leon@gmail.com>
2025-09-13 18:02:48 +0200

    Ajuste html v2

--- docs/assets/nuevos-puestos-DfFth_qh.js
+++ docs/assets/nuevos-puestos-DfFth_qh.js
@@ -1,10 +1,10 @@
-import{s as l,p as a}from"./style-CZqZCQWb.js";l();let d="",s=1;a.forEach(t=>{t[31].split(":")[0]=="NEW"&&(console.log({item:t}),d+=`
+import"./style-TBYWEVep.js";import{s as l,p as a}from"./githubStart-DQa3QPhl.js";l();let d="",s=1;a.forEach(t=>{t[31].split(":")[0]=="NEW"&&(console.log({item:t}),d+=`
             <tr>
                 <td class="td-text-small" scope="row">${s}</td>
                 <td class="td-text-small">${t[30]}</td>
                 <td class="td-text-large">${t[1]}</td>
                 <td class="td-text-large">${t[5]}</td>
-                <td class="td-text-small">${t[17]}</td>
+                <td class="td-text-small">${t[19]}</td>
                 <td class="td-text-small">${t[14]}</td>
                 <td class="td-text-small">${t[15]}</td>
                 <td class="td-text-large">${t[13]}</td>
